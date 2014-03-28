a8a3364b89dccf7a17540c6f88e0f60eebed1836
Tim Cameron Ryan <id@timryan.org>
2014-03-28 10:31:26 -0400

    Specify filename from command line? it autosaves.

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+RUNPWD=$(pwd)
 cd $(dirname $0)
 cd $(dirname $(readlink $0))
-./node_modules/.bin/nodewebkit $(pwd)
+./node_modules/.bin/nodewebkit $(pwd) "$RUNPWD" $@ > /dev/null 2>&1 &
